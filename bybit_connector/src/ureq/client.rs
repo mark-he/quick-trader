8a610ce7dee31f38b27fbd6b2e4c6f3ab1e352ee
minghua.he <minghua.he@hotmail.com>
2024-11-29 20:33:09 +0800

    deb

--- bybit_connector/src/ureq/client.rs
+++ bybit_connector/src/ureq/client.rs
@@ -102,12 +102,7 @@ impl BybitHttpClient {
                     },
                     crate::http::Method::Post => {
                         //timestamp+api_key+recv_window+raw_request_body
-                        payload = format!("{}{}{}{}{}", timestamp, api_key, recv_window, ureq_request
-                        .request_url()
-                        .unwrap()
-                        .as_url()
-                        .query()
-                        .unwrap(), body);
+                        payload = format!("{}{}{}{}{}", timestamp, api_key, recv_window, body);
                     },
                     _ => {
                         payload = "".to_string();
