cd8774fc48ae6da286360b04899c0a9ce0fe874a
minghua.he <minghua.he@hotmail.com>
2024-12-12 10:36:23 +0800

    deb

--- bybit/src/lib.rs
+++ bybit/src/lib.rs
@@ -3,3 +3,7 @@ pub mod bb_trade_server;
 pub mod model;
 pub mod bb_sim_market_server;
 pub mod bb_sim_trade_server;
+
+pub fn enable_prod(enabled: bool) {
+    bybit_connector::config::enable_prod(enabled);
+}
